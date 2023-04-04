bfe71a17b0a6fdf9b7e5198cd48e849f5efc07fb
gerwasiy <gerwasiy@gmail.com>
2023-04-04 09:49:31 +0300

    feat(testCard): created the testCard component

--- components/testCard.jsx
+++ components/testCard.jsx
@@ -1,9 +1,16 @@
-export default function TestCard() {
+import Link from 'next/link';
+import styles from '../styles/components/test-card.module.css'
+
+export default function TestCard({testName,description,path}) {
   return (
-    <div className="">
-      <h2>title</h2>
-      <div>image/desc</div>
-      <button>Start</button>
+    <div className={styles.card}>
+      <h2 className={styles.card__title}>{testName}</h2>
+      <p className={styles.card__description}>
+        {description}
+      </p>
+      <Link href={path} className={styles.card__button}>
+        Розпочати тест
+      </Link>
     </div>
   );
 }

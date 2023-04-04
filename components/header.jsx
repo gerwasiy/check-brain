2c524b158e8207fb522d3ffdde9e48dc2fde95de
gerwasiy <gerwasiy@gmail.com>
2023-04-04 09:47:12 +0300

    feat(header): add navigation bar and logo

--- components/header.jsx
+++ components/header.jsx
@@ -1,12 +1,34 @@
-import Link from "next/link";
 import styles from "../styles/components/header.module.css";
+import logo from "../public/logo.svg";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+import Image from "next/image";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
   return (
     <header className={styles.container}>
-      <div>logo</div>
-      <h1  className={styles.title}>Check Brain</h1>
-      <Link href='Profile'>Profile</Link>
+      <Link className={styles.navigationEl} href="/">
+        <FontAwesomeIcon icon={faHouse} />
+        <span>Home</span>
+      </Link>
+
+      <h1 className={styles.title}>
+        Check
+        <Image
+          className={styles.logo}
+          alt="logo"
+          width={100}
+          height={100}
+          src={logo}
+        />
+        Brain
+      </h1>
+      <Link className={styles.navigationEl} href="/Profile">
+        <FontAwesomeIcon icon={faUser} />
+        <span>Profile</span></Link>
     </header>
   );
 }

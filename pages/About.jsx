85bdaa6c72f477164a8f017f3fa976554de5d09c
gerwasiy <gerwasiy@gmail.com>
2023-05-27 17:24:22 +0300

    create about page

---
+++ pages/About.jsx
@@ -0,0 +1,16 @@
+import Footer from "../components/footer";
+import Header from "../components/header";
+
+export default function About() {
+  return (
+    <>
+      <Header />
+      <main>
+        <section>
+            
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+}

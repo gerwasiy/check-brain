8d906bf683e04bbfa383cc8a07c4f45bcd7c50ac
gerwasiy <gerwasiy@gmail.com>
2023-05-27 17:41:57 +0300

    change path for styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { AuthProvider } from '../contexts/AuthContext'
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false

480348750c2d356cce2a5724510883d5bc2ff66e
gerwasiy <gerwasiy@gmail.com>
2023-05-27 17:24:49 +0300

    create statistics page

---
+++ pages/Statistics.jsx
@@ -0,0 +1,15 @@
+import Footer from "../components/footer";
+import Header from "../components/header";
+
+export default function Statistics() {
+  return (
+    <>
+      <Header />
+      <main>
+        <section>
+            
+        </section>
+      </main>
+     <Footer/>
+    </>)
+}

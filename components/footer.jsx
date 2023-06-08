7eee0a49ab7b6fea60f18996a3e39895821b75c1
gerwasiy <gerwasiy@gmail.com>
2023-06-08 21:24:21 +0300

    feat:adding links to social media

--- components/footer.jsx
+++ components/footer.jsx
@@ -6,10 +6,10 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 export default function Footer() {
   return (
     <footer className={styles.footer__container}>
-      <a href="#"> <FontAwesomeIcon icon={faInstagram} /></a>
-      <a href="#"><FontAwesomeIcon icon={faGithub} /></a>
-      <a href="#"><FontAwesomeIcon icon={faTelegram} /></a>
-      <a href="#"><FontAwesomeIcon icon={faEnvelope} /></a>
+      <a href="https://www.instagram.com/gerwasiy/"> <FontAwesomeIcon icon={faInstagram} /></a>
+      <a href="https://github.com/gerwasiy"><FontAwesomeIcon icon={faGithub} /></a>
+      <a href="https://t.me/Iarik_sh"><FontAwesomeIcon icon={faTelegram} /></a>
+      <a href="mailto:yaroslav.shaposhnyk@nure.ua"><FontAwesomeIcon icon={faEnvelope} /></a>
       <a href="#"><FontAwesomeIcon icon={faTwitter} /></a>
     </footer>
   );

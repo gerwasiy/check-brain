92ac9c1397e1109215c7e7d35734e5b68eea8661
gerwasiy <gerwasiy@gmail.com>
2023-11-11 12:22:17 +0200

    update footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -6,11 +6,14 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 export default function Footer() {
   return (
     <footer className={styles.footer__container}>
+      <div>
       <a href="https://www.instagram.com/gerwasiy/"> <FontAwesomeIcon icon={faInstagram} /></a>
       <a href="https://github.com/gerwasiy"><FontAwesomeIcon icon={faGithub} /></a>
       <a href="https://t.me/Iarik_sh"><FontAwesomeIcon icon={faTelegram} /></a>
       <a href="mailto:yaroslav.shaposhnyk@nure.ua"><FontAwesomeIcon icon={faEnvelope} /></a>
       <a href="#"><FontAwesomeIcon icon={faTwitter} /></a>
+      </div>
+      <span>Copyright 2023 Check Brain</span>
     </footer>
   );
 }
